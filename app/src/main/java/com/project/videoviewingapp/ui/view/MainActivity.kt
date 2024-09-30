d721e2c1dc07317f0a89eea7a85cfac36afdefea
hudcom <denia0202@ukr.net>
2024-09-30 16:32:17 +0300

    Add MainActivity design, SecondActivity for video playback, item design.

--- app/src/main/java/com/project/videoviewingapp/ui/view/MainActivity.kt
+++ app/src/main/java/com/project/videoviewingapp/ui/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.project.videoviewingapp
+package com.project.videoviewingapp.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.project.videoviewingapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/project/videoviewingapp/ui/view/VideoPlayerActivity.kt
@@ -0,0 +1,12 @@
+package com.project.videoviewingapp.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.project.videoviewingapp.R
+
+class VideoPlayerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_video_player)
+    }
+}
