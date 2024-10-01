051eed12d6300ab6f0089daaf45519a0f813b56b
hudcom <denia0202@ukr.net>
2024-10-01 13:39:10 +0300

    Changed the code version in Kotlin and Gradle to add Dagger. Added dependencies to work with Retrofit and Dagger

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.android") version "1.9.0"
+    id("kotlin-kapt")
 }
 
 android {
@@ -44,7 +45,13 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
-
-
     implementation("com.google.android.material:material:1.12.0")
+
+    // Залежності для Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Залежності для Dagger 2
+    implementation("com.google.dagger:dagger:2.51")
+    kapt("com.google.dagger:dagger-compiler:2.51")
 }
